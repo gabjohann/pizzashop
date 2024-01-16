ebd32b65384453423fbd6418078cb5965297021f
Lucas Gabriel <gabif.johann@gmail.com>
2024-01-16 00:00:02 -0300

    add: env url schema

---
+++ src/env.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  VITE_API_URL: z.string().url(),
+})
+
+// import.meta -> de onde o vite trás as variáveis ambiente
+export const env = envSchema.parse(import.meta.env)
