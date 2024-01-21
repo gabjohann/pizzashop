f6c3bf5f5a1dcfaaf99f048a5d7ed057c928ae4a
Lucas Gabriel <gabif.johann@gmail.com>
2024-01-21 00:19:07 -0300

    feat: order details skeleton

--- pizzashop-web/src/pages/app/order-details.tsx
+++ pizzashop-web/src/pages/app/order-details.tsx
@@ -20,6 +20,7 @@ import {
 } from '@/components/ui/table'
 
 import { OrderStatus } from '../../components/order-status'
+import { OrderDetailsSkeleton } from './order-details-skeleton'
 
 interface OrderDetailsProps {
   orderId: string
@@ -40,7 +41,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
 
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -144,6 +145,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   )
